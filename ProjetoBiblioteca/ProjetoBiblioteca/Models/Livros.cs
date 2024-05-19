7a3e2fa2cdb0f79ce6397133c645eade2d8799df
MatheusFeliphe1997 <163544208+MatheusFeliphe1997@users.noreply.github.com>
2024-05-19 20:23:37 -0300

    Contrutores
    contrutores livros

--- ProjetoBiblioteca/ProjetoBiblioteca/Models/Livros.cs
+++ ProjetoBiblioteca/ProjetoBiblioteca/Models/Livros.cs
@@ -1,12 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProjetoBiblioteca.Models;
+
 
 public class Livros
 {
-   public int Id { get; set; }
+    public Livros ()
+    {   
+        Id = Guid.NewGuid().ToString();
+        CriadoEm = DateTime.Now;
+    }
+
+    public Livros 
+        (string titulo, string autor, int anoPublicacao, string genero, int exemplaresDisponiveis)
+    {
+        Titulo = titulo;
+        Autor = autor;
+        AnoPublicacao = anoPublicacao;
+        Genero = genero;
+        ExemplaresDisponiveis = exemplaresDisponiveis;
+        Id = Guid.NewGuid().ToString();
+        CriadoEm = DateTime.Now;
+
+    }
+
+
+
+    public string? Id { get; set; }
     public string? Titulo { get; set; }
     public string? Autor { get; set; }
     public int AnoPublicacao { get; set; }
     public string? Genero { get; set; }
     public int ExemplaresDisponiveis { get; set; }
+   
+    public DateTime CriadoEm { get; set; }
 
 }
