9b71e6441a35b7c7b63e947c9e19ccc67945cced
Lucas Huzar <lucashuzar@outlook.com>
2024-05-19 19:04:34 -0300

    Implementando Program.cs

--- ProjetoBiblioteca/ProjetoBiblioteca/Models/Emprestimo.cs
+++ ProjetoBiblioteca/ProjetoBiblioteca/Models/Emprestimo.cs
@@ -5,9 +5,9 @@ public class Emprestimo
 
     public int Id { get; set; }
     public int LivroId { get; set; }
-    public Livros livro { get; set; }
+    public Livros? livro { get; set; }
     public int UsuarioId { get; set; }
-    public Usuario usuario { get; set; }
+    public Usuario? usuario { get; set; }
     public DateTime DataEmprestimo { get; set; }
     public DateTime? DataDevolucao { get; set; }
 }
--- ProjetoBiblioteca/ProjetoBiblioteca/Models/Livros.cs
+++ ProjetoBiblioteca/ProjetoBiblioteca/Models/Livros.cs
@@ -4,10 +4,10 @@ public class Livros
 {
 
    public int Id { get; set; }
-    public string Titulo { get; set; }
-    public string Autor { get; set; }
+    public string? Titulo { get; set; }
+    public string? Autor { get; set; }
     public int AnoPublicacao { get; set; }
-    public string Genero { get; set; }
+    public string? Genero { get; set; }
     public int ExemplaresDisponiveis { get; set; }
 
 }
