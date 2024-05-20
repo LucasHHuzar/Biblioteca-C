ee15c4a09a4ab9cc9ff93c0f45ee8b3c1f0742d0
MatheusFeliphe1997 <163544208+MatheusFeliphe1997@users.noreply.github.com>
2024-05-19 22:39:35 -0300

    Update Devolucao.cs
    ok

--- ProjetoBiblioteca/ProjetoBiblioteca/Models/Devolucao.cs
+++ ProjetoBiblioteca/ProjetoBiblioteca/Models/Devolucao.cs
@@ -2,8 +2,6 @@ namespace ProjetoBiblioteca.Models;
 
 public class Devolucao
 {
-    
-
     public string? Id { get; set; }
     public Livros? Livro { get; set; }
     public Usuario? Usuario { get; set; }
