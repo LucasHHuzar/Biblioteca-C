ff1e5e81a4b1171898ef63f68a7562df868a7f7f
Lucas Huzar <lucashuzar@outlook.com>
2024-05-19 23:34:42 -0300

    Alterações finais

--- ProjetoBiblioteca/ProjetoBiblioteca/Models/AppDataContext.cs
+++ ProjetoBiblioteca/ProjetoBiblioteca/Models/AppDataContext.cs
@@ -13,19 +13,20 @@ public class AppDataContext : DbContext
     {
         optionsBuilder.UseSqlite("Data Source=banco_biblioteca.db");
     }
-    //  protected override void OnModelCreating(ModelBuilder modelBuilder)
-    //     {
-    //         // Configurar relacionamento entre Usuario e Emprestimo
-    //         modelBuilder.Entity<Emprestimo>()
-    //             .HasOne(e => e.Usuario)
-    //             .WithMany(u => u.Emprestimos)
-    //             .HasForeignKey(e => e.UsuarioId);
 
-    //         // Configurar relacionamento entre Livros e Emprestimo
-    //         modelBuilder.Entity<Emprestimo>()
-    //             .HasOne(e => e.Livro)
-    //             .WithMany()
-    //             .HasForeignKey(e => e.LivroId);
-    //     }
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configurar relacionamento entre Usuario e Emprestimo
+            modelBuilder.Entity<Emprestimo>()
+                .HasOne(e => e.Usuario)
+                .WithMany(u => u.Emprestimos)
+                .HasForeignKey(e => e.UsuarioId);
+
+            // Configurar relacionamento entre Livros e Emprestimo
+            modelBuilder.Entity<Emprestimo>()
+                .HasOne(e => e.Livro)
+                .WithMany()
+                .HasForeignKey(e => e.LivroId);
+        }
 
 }
--- ProjetoBiblioteca/ProjetoBiblioteca/Models/Emprestimo.cs
+++ ProjetoBiblioteca/ProjetoBiblioteca/Models/Emprestimo.cs
@@ -16,6 +16,7 @@ public class Emprestimo
     {   
         LivroId = livroId;
         UsuarioId = usuarioId;
+        DataEmprestimo = DateTime.Now;
     }
     
 
