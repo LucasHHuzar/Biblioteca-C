84f2095149805eacc216162f5d4d6a9c3d260200
Lucas Huzar <lucashuzar@outlook.com>
2024-06-29 16:30:23 -0300

    Arrumando front

--- ProjetoBiblioteca/frontend/livraria-app/src/services/api.ts
+++ ProjetoBiblioteca/frontend/livraria-app/src/services/api.ts
@@ -1,12 +1,12 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5077/API', // ajuste conforme a porta usada pela sua API
+  baseURL: 'http://localhost:5077/api', // ajuste conforme a porta usada pela sua API
 });
 
-export const getLivros = () => api.get('./components/LivrosListar');
-export const getUsuarios = () => api.get('./components/UsuarioListar');
-export const addLivro = (livro: any) => api.post('./components/AddLivro', livro);
-export const addUsuario = (usuario: any) => api.post('./components/AddUsuario', usuario);
+export const getLivros = () => api.get('/livros/listar');
+export const getUsuarios = () => api.get('/usuario/listar');
+export const addLivro = (livro: any) => api.post('/livros/cadastrar', livro);
+export const addUsuario = (usuario: any) => api.post('/usuario/cadastrar', usuario);
 
 export default api;
